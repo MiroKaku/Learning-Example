bc4602c3a8ba308d0fddea5f37b2b5efe501c383
Miro.Kaku <50670906+MiroKaku@users.noreply.github.com>
2019-06-01 11:26:45 +0800

    [ADD] ClearVSProject PowerShell Script

---
+++ ClearVSProject.ps1
@@ -0,0 +1,11 @@
+﻿$shell  = New-Object -COM Shell.Application
+$files  = Get-ChildItem "." -Include ".vs","Output" -Force -Recurse
+
+foreach ($file in $files)
+{
+    $path = Resolve-Path "$file";
+    Write-Host "delete " "$path";
+    $shell.NameSpace(10).MoveHere("$path");
+}
+
+Read-Host -Prompt "Press Enter to continue"
